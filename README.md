71913e31474b6cb2de61ffa2c43b4a3ae1cc221c
Hans Chaudry <hanschaudry@outlook.com>
2023-01-20 09:39:52 -0500

    finished readme

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 <body style="background-color: white; color: black">
 
-<h1><img src="YT-DownloaderLogo.icns" alt="drawing" width="60" height="60"/> YT Video Downloader</h1>
+<h1 style="text-align: center"><img src="YT-DownloaderLogo.icns" alt="drawing" width="60" height="60"/> YT Video Downloader</h1>
 
-<p style="color: black"> 
+<p style="color: black; text-align: center"> 
     Desktop application that allows the user to download YouTube videos directly to their computer. 
     Developed with the use of python's front-end libraries tkinter, customtkinter, and with the use pytube on the 
     back-end.
 </p>
 
-<figure>
-    <p align="center"><img src="images/SC1.png" alt="drawing" width="500"/></p>
+<figure style="text-align: center">
+    <p align="center"><img src="images/SC1.png" alt="drawing" width="600"/></p>
     <figcaption>
         The user inputs the link to the video they wish to download. Then they click on browse, which 
         opens a finder window that allows them to select a directory path to store the MP4 file. Lastly, they 
@@ -18,12 +18,12 @@
     </figcaption>
 </figure>
 
-<figure>
-    <p align="center"><img src="images/SC2.png" alt="drawing" width="350"/>
-    <img src="images/SC3.png" alt="drawing" width="350"/></p>
+<figure style="text-align: center">
+    <p align="center"><img src="images/SC2.png" alt="drawing" width="400"/>
+    <img src="images/SC3.png" alt="drawing" width="400"/></p>
     <figcaption>
         When and invalid url is entered a message window will open to notify the user. In addition to invalid URLs, 
-a valid URL may not belong to a YouTube video and a message is also sent in that instance.
+        a valid URL may not belong to a YouTube video and a message is also sent in that instance.
     </figcaption>
 </figure>
 
--- update.py
+++ update.py
@@ -13,9 +13,8 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.title("YT Downloader")
-        self.geometry(f"{600}x{350}")
-        self.minsize(600, 350)
-        self.maxsize(600, 350)
+        self.geometry(f"{800}x{450}")
+        self.minsize(770, 480)
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
@@ -28,6 +27,14 @@ class App(customtkinter.CTk):
                                                   font=customtkinter.CTkFont(size=25, weight="bold"))
         self.title_label.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
+        # HISTORY FRAME
+        self.history_frame = customtkinter.CTkFrame(self, width=225, corner_radius=0)
+        self.history_frame.grid(row=1, column=0, rowspan=4, sticky="nsew")
+        self.history_frame.grid_rowconfigure(4, weight=1)
+        self.history_label = customtkinter.CTkLabel(self.history_frame, text="Download History",
+                                                    font=customtkinter.CTkFont(size=18, weight="bold"))
+        self.history_label.grid(row=0, column=0, padx=20, pady=(10, 10))
+
         # MAIN FRAME
         self.main_frame = customtkinter.CTkFrame(self, corner_radius=0)
         self.main_frame.grid(row=1, column=1, columnspan=4, rowspan=2, sticky="nsew")
@@ -39,16 +46,16 @@ class App(customtkinter.CTk):
         self.path_entry = customtkinter.CTkEntry(master=self.main_frame, width=410, height=30,
                                                  border_width=2, justify='center', state='disabled',
                                                  textvariable=self.download_path)
-        self.path_entry.place(relx=0.43, rely=0.57, anchor=tkinter.CENTER)
+        self.path_entry.place(relx=0.42, rely=0.55, anchor=tkinter.CENTER)
         self.path_btn = customtkinter.CTkButton(master=self.main_frame, width=80, height=30, border_width=0,
                                                 corner_radius=8, text="browse", command=self.browseFolders)
 
-        self.path_btn.place(relx=0.845, rely=0.57, anchor=tkinter.CENTER)
+        self.path_btn.place(relx=0.845, rely=0.55, anchor=tkinter.CENTER)
 
         self.download_btn = customtkinter.CTkButton(master=self.main_frame, width=80, height=30, border_width=0,
                                                     corner_radius=8, text="Download", command=self.downloadVideo)
 
-        self.download_btn.place(relx=0.5, rely=0.69, anchor=tkinter.CENTER)
+        self.download_btn.place(relx=0.5, rely=0.65, anchor=tkinter.CENTER)
 
     def browseFolders(self, *args):
         download_dir = tkinter.filedialog.askdirectory()
@@ -66,7 +73,7 @@ class App(customtkinter.CTk):
             except:
                 tkinter.messagebox.showinfo(title=None, message=("Invalid URL entered. Please make sure that it is a "
                                                                  "URL to a YouTube video, not a playlist, channel, "
-                                                                 "or YouTube homepage "))
+                                                                 "or the YouTube homepage "))
             get_stream = self.getQualityStream(get_video)
             get_stream.download(folder)
             tkinter.messagebox.showinfo(title=None, message=("Video was successfully downloaded. The file can be found"
