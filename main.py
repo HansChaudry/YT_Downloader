92dc00fedf89d1844667f67c7c81f9c8354fc1ad
Hans Chaudry <hanschaudry@outlook.com>
2023-09-18 14:00:35 -0400

    updated pytube package and disabled resizing

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import customtkinter
 import validators
 from pytube import YouTube
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+# Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("System")
+# Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("blue")
 
 
 class App(customtkinter.CTk):
@@ -16,13 +18,15 @@ class App(customtkinter.CTk):
         self.geometry(f"{600}x{350}")
         self.minsize(600, 350)
         self.maxsize(600, 350)
+        self.resizable(0, 0)
 
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((1, 2), weight=1)
 
         # TITLE FRAME
-        self.title_frame = customtkinter.CTkFrame(self, width=225, height=45, corner_radius=0)
+        self.title_frame = customtkinter.CTkFrame(
+            self, width=225, height=45, corner_radius=0)
         self.title_frame.grid(row=0, column=1, columnspan=4, sticky="nsew")
         self.title_label = customtkinter.CTkLabel(self.title_frame, text="YT Downloader",
                                                   font=customtkinter.CTkFont(size=25, weight="bold"))
@@ -30,7 +34,8 @@ class App(customtkinter.CTk):
 
         # MAIN FRAME
         self.main_frame = customtkinter.CTkFrame(self, corner_radius=0)
-        self.main_frame.grid(row=1, column=1, columnspan=4, rowspan=2, sticky="nsew")
+        self.main_frame.grid(row=1, column=1, columnspan=4,
+                             rowspan=2, sticky="nsew")
         self.link_entry = customtkinter.CTkEntry(master=self.main_frame, placeholder_text="Youtube Link", width=500,
                                                  height=30, border_width=2, justify='center')
         self.link_entry.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
@@ -58,7 +63,8 @@ class App(customtkinter.CTk):
         vidLink = self.link_entry.get()
 
         if not validators.url(vidLink):
-            tkinter.messagebox.showinfo(title=None, message="The URL entered is invalid")
+            tkinter.messagebox.showinfo(
+                title=None, message="The URL entered is invalid")
         else:
             folder = self.download_path.get()
             try:
@@ -72,10 +78,10 @@ class App(customtkinter.CTk):
             tkinter.messagebox.showinfo(title=None, message=("Video was successfully downloaded. The file can be found"
                                                              " at " + self.download_path.get()))
 
-    def getQualityStream(self, link: object) -> None:
+    def getQualityStream(self, link: str) -> None:
         resolutions = ["1080p", "720p", "480", "360p", "240p"]
-        for reso in resolutions:
-            streams = link.streams.filter(res=reso, progressive=True)
+        for resolution in resolutions:
+            streams = link.streams.filter(res=resolution, progressive=True)
             if len(streams) != 0:
                 return streams.first()
 
