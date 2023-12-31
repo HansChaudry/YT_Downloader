1ba0e21a22dcdd3216c42418affb18e0aa65ecd2
Hans Chaudry <hanschaudry@gmail.com>
2023-12-31 16:24:18 -0500

    refactored code and added cli client for distribution on test.pypip

---
+++ cli-setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name = 'YT_Installer',
+    version = '0.0.5',
+    author = 'Hans Chaudry',
+    author_email = 'hanschaudry@outlook.com',
+    license = '<the license you chose>',
+    description = 'CLI tool that allows yu to download a youtube video to a choosen directory',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/HansChaudry/YT_Downloader',
+    py_modules = ['CLI', 'app'],
+    packages = find_packages(),
+    install_requires = [requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = '''
+        [console_scripts]
+        yt=CLI:cli
+    '''
+)
